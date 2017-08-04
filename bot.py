d6ff64fca9bdbc8cd77e633b4756a3e51869e1f4
Koji ONO <naninuneter55@gmail.com>
2017-08-05 07:55:06 +0900

    accept return 404

--- bot.py
+++ bot.py
@@ -35,6 +35,23 @@ def callback():
 
     return 'OK'
 
+@app.route("/", methods=['POST'])
+def root():
+    print("=== root ===")
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
