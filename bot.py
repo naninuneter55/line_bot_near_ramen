1488775605aa3db309d56472602f180e2b21100a
Koji ONO <naninuneter55@gmail.com>
2017-08-08 21:14:18 +0900

    add location event

--- bot.py
+++ bot.py
@@ -74,15 +74,15 @@ def handle_location(event):
             title=rest['name'],
             text=rest['name'],
             actions=[
-                PostbackTemplateAction(
-                    label='postback1',
-                    text='postback text1',
-                    data='action=buy&itemid=1'
-                ),
-                MessageTemplateAction(
-                    label='message1',
-                    text='message text1'
-                ),
+                # PostbackTemplateAction(
+                #     label='postback1',
+                #     text='postback text1',
+                #     data='action=buy&itemid=1'
+                # ),
+                # MessageTemplateAction(
+                #     label='message1',
+                #     text='message text1'
+                # ),
                 URITemplateAction(
                     label='uri1',
                     uri='http://example.com/1'
