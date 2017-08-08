40c988b20ff2964b96a4cfd1e1659630efaa417f
Koji ONO <naninuneter55@gmail.com>
2017-08-08 21:39:20 +0900

    add location event

--- bot.py
+++ bot.py
@@ -101,48 +101,49 @@ def handle_location(event):
     carousel_template_message = TemplateSendMessage(
         alt_text='Carousel template',
         template=CarouselTemplate(
-            columns=[
-                CarouselColumn(
-                    thumbnail_image_url='https://example.com/item1.jpg',
-                    title='this is menu1',
-                    text='description1',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='postback1',
-                            text='postback text1',
-                            data='action=buy&itemid=1'
-                        ),
-                        MessageTemplateAction(
-                            label='message1',
-                            text='message text1'
-                        ),
-                        URITemplateAction(
-                            label='uri1',
-                            uri='http://example.com/1'
-                        )
-                    ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url='https://example.com/item2.jpg',
-                    title='this is menu2',
-                    text='description2',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='postback2',
-                            text='postback text2',
-                            data='action=buy&itemid=2'
-                        ),
-                        MessageTemplateAction(
-                            label='message2',
-                            text='message text2'
-                        ),
-                        URITemplateAction(
-                            label='uri2',
-                            uri='http://example.com/2'
-                        )
-                    ]
-                )
-            ]
+            columns=c_cols
+            # columns=[
+            #     CarouselColumn(
+            #         thumbnail_image_url='https://example.com/item1.jpg',
+            #         title='this is menu1',
+            #         text='description1',
+            #         actions=[
+            #             PostbackTemplateAction(
+            #                 label='postback1',
+            #                 text='postback text1',
+            #                 data='action=buy&itemid=1'
+            #             ),
+            #             MessageTemplateAction(
+            #                 label='message1',
+            #                 text='message text1'
+            #             ),
+            #             URITemplateAction(
+            #                 label='uri1',
+            #                 uri='http://example.com/1'
+            #             )
+            #         ]
+            #     ),
+            #     CarouselColumn(
+            #         thumbnail_image_url='https://example.com/item2.jpg',
+            #         title='this is menu2',
+            #         text='description2',
+            #         actions=[
+            #             PostbackTemplateAction(
+            #                 label='postback2',
+            #                 text='postback text2',
+            #                 data='action=buy&itemid=2'
+            #             ),
+            #             MessageTemplateAction(
+            #                 label='message2',
+            #                 text='message text2'
+            #             ),
+            #             URITemplateAction(
+            #                 label='uri2',
+            #                 uri='http://example.com/2'
+            #             )
+            #         ]
+            #     )
+            # ]
         )
     )
 
